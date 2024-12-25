4a1459a4e6632a545bc97212545df8ad8ded8d44
Artemiy2001 <art.barkovskii@mail.ru>
2024-12-25 22:00:10 +0300

    Добавил в дао методы для взаимодействия с таблицами, появилась ошибка с Connection

--- src/com/artem/training/store/dao/BuyerDao.java
+++ src/com/artem/training/store/dao/BuyerDao.java
@@ -6,6 +6,7 @@ import com.artem.training.store.utils.conect_utils.TakeConnection;
 
 import javax.annotation.processing.Generated;
 import java.sql.*;
+import java.util.Optional;
 
 public class BuyerDao {
 
@@ -25,6 +26,94 @@ public class BuyerDao {
             VALUES (?, ?, ?)
             
             """;
+
+    private static final String UPDATE_SQL = """
+            UPDATE buyer
+            SET user_name = ?,
+                address = ?,
+                password = ?
+            WHERE id = ?
+            
+            """;
+
+    private static final String FIND_BY_ID_SQL = """
+            SELECT id, user_name, address, password
+            FROM buyer
+            WHERE id = ?
+            """;
+
+    private static final String FIND_BY_NAME_SQL = """
+            
+            SELECT id, user_name, address, password
+            FROM buyer
+            WHERE user_name = ?
+            """;
+
+    public Optional<Buyer> findByName(String user_name) {
+        try (Connection connection = TakeConnection.connection;
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_NAME_SQL)) {
+
+            preparedStatement.setString(1, user_name);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            Buyer buyer = null;
+
+            if (resultSet.next()) {
+                buyer = new Buyer(
+                        resultSet.getInt("id"),
+                        resultSet.getString("user_name"),
+                        resultSet.getString("address"),
+                        resultSet.getString("password")
+                );
+            }
+
+            return Optional.ofNullable(buyer);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<Buyer> findById(int id) {
+        try (Connection connection = TakeConnection.connection;
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            Buyer buyer = null;
+
+            if (resultSet.next()) {
+                buyer = new Buyer(
+                        resultSet.getInt("id"),
+                        resultSet.getString("user_name"),
+                        resultSet.getString("address"),
+                        resultSet.getString("password")
+                );
+            }
+
+            return Optional.ofNullable(buyer);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateBuyer(Buyer buyer) {
+
+        try (Connection connection = TakeConnection.connection;
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
+            preparedStatement.setString(1, buyer.getName());
+            preparedStatement.setString(2, buyer.getAddress());
+            preparedStatement.setString(3, buyer.getPassword());
+            preparedStatement.setLong(4, buyer.getId());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     public boolean deleteBuyer(int id) {
--- src/com/artem/training/store/utils/check_utils/CheckUsername.java
+++ src/com/artem/training/store/utils/check_utils/CheckUsername.java
@@ -1,40 +1,26 @@
 package com.artem.training.store.utils.check_utils;
 
+import com.artem.training.store.dao.BuyerDao;
+import com.artem.training.store.entity.Buyer;
 import com.artem.training.store.utils.conect_utils.ConnectionManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class CheckUsername {
 
     public static boolean checkToUnique(String userName){
 
-        String sql = """
-            select * from buyer
-            where user_name = ?
-        """;
+        BuyerDao buyerDao = BuyerDao.getInstance();
+
+        Optional<Buyer> maybeBuyer = buyerDao.findByName(userName);
+
+        return maybeBuyer.isEmpty();
 
 
-        try {
-            try (Connection connection = ConnectionManager.open()) {
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setString(1, userName);
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-
-                if (resultSet.next()) {
-                    return false;
-                }else {
-                    return true;
-                }
-
-
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
 
 
     }
